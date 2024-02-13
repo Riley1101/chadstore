62a6684623c0ac7ea09dc1528d44570cea745bc4
Riley1101 <arkardev00@gmail.com>
2024-02-12 19:28:05 -0500

    cart now show itesm

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ApplicationContext } from "@/providers/ApplicationContext";
 import { CartItem } from "./CartItem";
-import { useCart } from "medusa-react";
+import { useGetCart } from "medusa-react";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 import {
@@ -22,28 +23,38 @@ type Props = {
 };
 
 export function Cart({ isOpen, setOpen }: Props) {
-  const { cart } = useCart();
-  console.log(cart?.items)
+  const cartContext = useContext(ApplicationContext);
+  const cartId = cartContext?.cart?.cartId || "";
+  const { cart } = useGetCart(cartId);
+  console.log(cart)
+
   return (
     <Drawer open={isOpen} onOpenChange={setOpen}>
       <DrawerTrigger asChild>
-        <Button variant="outline">
+        <Button variant="outline" className="relative">
           <ShoppingCartIcon size={20} />
+          {cart?.items && cart?.items.length > 0 && (
+            <span className="ml-2 bg-primary w-6 h-6 text-primary-foreground text-[12px] grid items-center rounded-full">
+              {cart?.items.length}
+            </span>
+          )}
         </Button>
       </DrawerTrigger>
-      <DrawerContent className="border px-4 lg:px-[20%]">
+      <DrawerContent className="border px-4 lg:px-[30%]">
         <DrawerHeader>
           <DrawerTitle>{!!!cart?.total ? "Empty cart" : "Items"}</DrawerTitle>
           <DrawerDescription>
-            {!!!cart?.total ? "Please add items to proceed" : " 3 Items"}
+            {!!!cart?.items
+              ? "Please add items to proceed"
+              : `${cart.items.length} items`}
           </DrawerDescription>
         </DrawerHeader>
         <ScrollArea className="flex flex-col p-4  w-full h-[400px]">
-          {cart?.items.map((i) => <CartItem key={i.id} />)}
+          {cart?.items.map((i) => <CartItem key={i.id} product={i} />)}
           <ScrollBar />
         </ScrollArea>
         <DrawerFooter className="flex items-center flex-row ">
-          <DrawerClose className="ml-auto">
+          <DrawerClose className="ml-auto" asChild>
             <Button variant="outline">Cancel</Button>
           </DrawerClose>
           <Button>Checkout Now</Button>
--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -2,22 +2,27 @@ import React from "react";
 import { PlusIcon, MinusIcon } from "lucide-react";
 import { AvatarImage, Avatar, AvatarFallback } from "./ui/avatar";
 import { Button } from "./ui/button";
+import type { LineItem } from "@medusajs/medusa";
 
-type Props = {};
+type Props = {
+  product: LineItem;
+};
 
-export function CartItem({}: Props) {
-  let [quantity, setQuantity] = React.useState(1);
+export function CartItem({ product }: Props) {
+  let [quantity, setQuantity] = React.useState(product.quantity);
   return (
     <div className="w-full mb-2 flex items-center">
       <div className=" w-full flex items-center space-x-4 rounded-md border p-4 flex-wrap">
         <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
+          {product.thumbnail && (
+            <AvatarImage src={product.thumbnail} className="object-cover" />
+          )}
+          <AvatarFallback>{product.title.charAt(0)}</AvatarFallback>
         </Avatar>
         <div className="flex-1 space-y-1 basis-[50%]">
-          <p className="text-sm font-medium leading-none">Push Notifications</p>
+          <p className="text-sm font-medium leading-none">{product?.title}</p>
           <p className="text-sm text-muted-foreground">
-            Send notifications to device.
+            {product?.description}
           </p>
         </div>
         <div className="mt-4 md:mt-0 flex gap-2 items-center">
